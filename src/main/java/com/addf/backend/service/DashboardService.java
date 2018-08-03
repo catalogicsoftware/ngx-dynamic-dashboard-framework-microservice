9cc79d6309ca162a534af4713d054be296b0a5f0
jhamilton <jhamilton@catalogicsoftware.com>
2018-08-03 15:57:36 -0400

    Adjusted the pom file to include a surefire plugin. Also removed the static content that houses the front end. The front end can be found in the ngx-dynamic-dashboard framework repository.

--- src/main/java/com/addf/backend/service/DashboardService.java
+++ src/main/java/com/addf/backend/service/DashboardService.java
@@ -3,6 +3,7 @@ package com.addf.backend.service;
 import com.google.gson.Gson;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.NaturalLanguageClassifier;
 import com.ibm.watson.developer_cloud.natural_language_classifier.v1.model.Classification;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.MediaType;
@@ -12,11 +13,14 @@ import com.addf.backend.service.connectiontester.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 
 @RestController
 @SpringBootApplication
 public class DashboardService {
+    @Value("${version.number}")
+    private String versionNumber;
 
     public static void main(String[] args) {
         SpringApplication.run(DashboardService.class, args);
@@ -53,117 +57,11 @@ public class DashboardService {
 
 
     @CrossOrigin
-    @RequestMapping("/donutDetail")
-    public ResponseObject getDonutDetails() {
+    @RequestMapping("/version")
+    public String getVersion() {
 
-
-        Tag tag = new Tag("VM", "staging");
-        List<Tag> tags = new ArrayList<>();
-        ResourceDetail resourceDetail = new ResourceDetail("0", "vm 1", "vm", tags);
-        List<ResourceDetail> resourceDetailList = new ArrayList<>();
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-
-        tag = new Tag("Site A", "department 1");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("1", "vm 1", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site A", "department 1");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("2", "vm 2", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site A", "department 1");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("3", "vm 3", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site B", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("4", "vm 4", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site B", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("5", "vm 5", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site B", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("6", "vm 6", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-
-        Resource resource = new Resource("compliant", "" + resourceDetailList.size(), resourceDetailList);
-        List<Resource> resourceList = new ArrayList<>();
-        resourceList.add(resource);
-
-
-        tag = new Tag("Site A", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("7", "vm 7", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site A", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("8", "vm 8", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site A", "department 1");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("9", "vm 9", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site C", "department 1");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("10", "vm 10", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-
-        resource = new Resource("staging", "" + resourceDetailList.size(), resourceDetailList);
-        resourceList = new ArrayList<>();
-        resourceList.add(resource);
-
-
-        tag = new Tag("Site A", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("11", "vm 11", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-        tag = new Tag("Site A", "department 2");
-        tags = new ArrayList<>();
-        resourceDetail = new ResourceDetail("12", "vm 12", "vm", tags);
-        tags.add(tag);
-        resourceDetailList.add(resourceDetail);
-
-
-        resource = new Resource("non-compliant", "" + resourceDetailList.size(), resourceDetailList);
-        resourceList = new ArrayList<>();
-        resourceList.add(resource);
-
-
-        Gson g = new Gson();
-        g.toJson(resourceList);
-
-        System.out.println(g.toString());
-
-
-        return null;
+        return this.versionNumber;
     }
-
 
 }
 
